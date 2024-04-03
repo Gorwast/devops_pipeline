ba85fec0a9d41e2320b133670f5143929693e9c4
Luis Lopez <lk23@live.com.mx>
2024-04-03 15:33:53 -0400

    Nuevo commit cambio test

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -12,7 +12,7 @@ public class BasicApplicationTests {
  
  @Test
  public void contextLoads() {
- 	    String meessage= "default messagess new cambio test devops";
+ 	    String meessage= "default messagess e new cambio test devops";
  	    Assert.assertNotNull(meessage);
  }
  
